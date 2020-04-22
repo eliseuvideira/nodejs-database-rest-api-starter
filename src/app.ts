11e328a2705eb0ff854ffa67067e12564f0122a1
Eliseu Videira <eliseuvideira1@gmail.com>
2020-04-21 21:46:03 -0300

    added morgan middleware to log requests

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,13 @@ import { json } from 'body-parser';
 import { readdirSync } from 'fs';
 import { join } from 'path';
 import { notFound, exception } from './middlewares/errors';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(cors());
 app.use(json());
+app.use(morgan('short'));
 
 const routes = readdirSync(join(__dirname, 'routes'));
 for (const route of routes) {
