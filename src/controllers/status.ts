0ebb635a8d2a97d4fdf83b2c53351da98325fe58
Eliseu Videira <eliseuvideira1@gmail.com>
2020-04-21 21:33:21 -0300

    added validation middleware and modified status route to showcase its use

--- src/controllers/status.ts
+++ src/controllers/status.ts
@@ -1,5 +1,11 @@
 import { RequestHandler } from 'express';
+import moment from 'moment';
 
-export const getStatus: RequestHandler = (_req, res, _next) => {
-  res.status(204).end();
+export const getStatus: RequestHandler = (req, res, _next) => {
+  const { results } = req.query;
+  if (results) {
+    res.status(200).json({ timestamp: moment() });
+  } else {
+    res.status(204).end();
+  }
 };
