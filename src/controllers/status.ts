0be3025351f70725a7c001cc4e625df088b36c76
Eliseu Videira <eliseuvideira1@gmail.com>
2020-06-28 00:21:35 -0300

    fixed status route, removed useless code

--- src/controllers/status.ts
+++ src/controllers/status.ts
@@ -1,11 +1,9 @@
 import { RequestHandler } from 'express';
-import moment from 'moment';
 
-export const getStatus: RequestHandler = (req, res, _next) => {
-  const { results } = req.query;
-  if (results) {
-    res.status(200).json({ timestamp: moment() });
-  } else {
+export const getStatus: RequestHandler = (req, res, next) => {
+  try {
     res.status(204).end();
+  } catch (err) {
+    next(err);
   }
 };
