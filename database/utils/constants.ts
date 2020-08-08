fffc67ea21fa40676b371a4c6626a6ce54d3b791
Eliseu Videira <eliseuvideira1@gmail.com>
2020-08-08 11:26:10 -0300

    Fixed env variable

--- database/utils/constants.ts
+++ database/utils/constants.ts
@@ -2,5 +2,8 @@ export const NODE_ENV = process.env.NODE_ENV || 'development';
 
 type DatabaseClient = 'pg' | 'sqlite3';
 
-export const DATABASE_CLIENT: DatabaseClient =
-  NODE_ENV === 'test' ? 'sqlite3' : 'pg';
+export const DATABASE_CLIENT: DatabaseClient = ['test', 'development'].includes(
+  NODE_ENV,
+)
+  ? 'sqlite3'
+  : 'pg';
