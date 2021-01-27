372e938f3c71272ba53038845b4e210cad72ae5f
Eliseu Videira <eliseuvideira1@gmail.com>
2021-01-27 16:12:55 -0300

    :package: upgraded packages

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -4,13 +4,13 @@ import { JWT_SECRET } from './constants';
 export const sign = async (token: Record<string, any>) =>
   new Promise<string>((resolve, reject) =>
     jwt.sign(token, JWT_SECRET, (err, token) =>
-      err ? reject(err) : resolve(token),
+      err ? reject(err) : resolve(token as any),
     ),
   );
 
 export const decode = async (token: string) =>
   new Promise<Record<string, any>>((resolve, reject) => {
     jwt.verify(token, JWT_SECRET, (err, data) =>
-      err ? reject(new Error('Invalid token')) : resolve(data),
+      err ? reject(new Error('Invalid token')) : resolve(data as any),
     );
   });
