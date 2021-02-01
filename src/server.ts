9475485d5445f09d58bd5d8f1385845b974269cb
Eliseu Videira <eliseuvideira1@gmail.com>
2021-02-01 10:35:15 -0300

    :wrench: simplified server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,17 +6,15 @@ import server from '@ev-fns/server';
 import app from './app';
 import { database } from './functions/database';
 
-const PORT = +(process.env.PORT || 0) || 3000;
-
 server(
   app,
-  PORT,
+  +(process.env.PORT || 0) || 3000,
   async () => {
     await database.raw('SELECT 1 AS server_status');
     await database.migrate.latest();
   },
   () => {
-    console.info(`listening at http://localhost:${PORT}`);
+    console.info(`listening at http://localhost:${process.env.PORT}`);
   },
 ).catch((err) => {
   console.error(err);
