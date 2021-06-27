e660e011d3cd4cae6029f6b9d1a7f400859d53dc
Eliseu Videira <eliseuvideira1@gmail.com>
2021-06-27 17:57:29 -0300

    :wrench: modified database structure

--- database/migrations/20210627115616_create_table_packages.js
+++ database/migrations/20210627115616_create_table_packages.js
@@ -8,10 +8,11 @@ exports.up = async (knex) => {
   await knex.schema.createTable("packages", (table) => {
     table.text("name").notNullable().primary();
     table.text("version").notNullable();
-    table.text("license").notNullable();
     table.text("description").notNullable();
-    table.text("homepage").notNullable();
+    table.text("license").notNullable();
     table.text("repository").notNullable();
+    table.text("homepage").notNullable();
+    table.text("node_version");
     table.integer("downloads").notNullable();
     table.dateTime("created_at").notNullable().defaultTo(knex.fn.now());
     table.dateTime("updated_at").notNullable().defaultTo(knex.fn.now());
